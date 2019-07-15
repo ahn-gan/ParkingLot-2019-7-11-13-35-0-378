1809feb35b54e6de7d67ac61793f0051a7c253fc
Akin <akin.gan@oocl.com>
2019-07-15 20:27:50 +0800

    [Akin]refactor: separate the given data of SuperSmartParkingBoyTest class as a method.

--- src/test/java/com/thoughtworks/tdd/SuperSmartParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/SuperSmartParkingBoyTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,26 +9,41 @@ import java.util.List;
 
 public class SuperSmartParkingBoyTest {
 
+    private SuperSmartParkingBoy superSmartParkingBoy;
+
+    private int capacity;
+
+    private Car car;
+
+    private ParkingLot firstParkingLot;
+
+    private ParkingLot secondParkingLot;
+
+    private List<ParkingLot> parkingLots;
+
+    @BeforeEach
+    void setUpData() {
+        capacity = 10;
+        car = new Car();
+        parkingLots = new ArrayList<>();
+        firstParkingLot = new ParkingLot(capacity);
+        secondParkingLot = new ParkingLot(capacity);
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+        superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+    }
+
     /*
      * *****************Story 5 **************************************
      * */
     @Test
     public void should_park_car_to_second_parking_lot_by_super_smart_boy_when_second_parking_lot_has_a_larger_available_position_rate() {
         // given
-        Car car = new Car();
-        ParkingLot firstParkingLot = new ParkingLot(10);
-        ParkingLot secondParkingLot = new ParkingLot(10);
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(firstParkingLot);
-        parkingLots.add(secondParkingLot);
         // parking 2 cars in firstParkingLot
         firstParkingLot.getParkingCarTicket().put(new Ticket(), new Car());
         firstParkingLot.getParkingCarTicket().put(new Ticket(), new Car());
-
         // parking 1 car in secondParkingLot
         secondParkingLot.getParkingCarTicket().put(new Ticket(), new Car());
-
-        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
 
         // when
         // park car by superSmartParkingBoy to the parkingLot which has a larger available position rate (positions available / total capacity
